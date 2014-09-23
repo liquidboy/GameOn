1cb1594e2eb3d536e6d3f9d59a883fbf0e8ddf99
Jose Fajardo <fajardo_jf@hotmail.com>
2014-09-23 21:48:10 +1000

    first checkin

---
+++ trunk/GameOnWeb/GameOnWeb/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace GameOnWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.LastRefreshed = AzureLib.NotificationCenter.Refresh();
+
+            return View();
+        }
+    }
+}
