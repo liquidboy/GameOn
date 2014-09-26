f0f63997d99b07934ac35626dc114541fc0e953d
Jose Fajardo <fajardo_jf@hotmail.com>
2014-09-26 20:38:44 +1000

    Layout page

--- trunk/ModernFx/Modern/BootUp.js
+++ trunk/ModernFx/Modern/BootUp.js
@@ -11,6 +11,17 @@ var BootUp = (function () {
 
     BootUp.prototype.Unload = function () {
     };
+
+    BootUp.prototype._getQueryVariable = function (variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+    };
     return BootUp;
 })();
 
