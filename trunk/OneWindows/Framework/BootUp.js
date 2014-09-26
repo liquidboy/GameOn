139e63ebaa7fb17ec0ffeba7033e4c8398f7e103
Jose Fajardo <fajardo_jf@hotmail.com>
2014-09-27 08:19:03 +1000

    gameon and bootup

---
+++ trunk/OneWindows/Framework/BootUp.js
@@ -0,0 +1,11 @@
+﻿var ModernFx;
+(function (ModernFx) {
+    var BootUp = (function () {
+        function BootUp($scope) {
+            $scope.statusText = "Hello from TypeScript + AngularJS";
+        }
+        return BootUp;
+    })();
+    ModernFx.BootUp = BootUp;
+})(ModernFx || (ModernFx = {}));
+//# sourceMappingURL=BootUp.js.map
--- trunk/OneWindows/Framework/BootUp.ts
+++ trunk/OneWindows/Framework/BootUp.ts
@@ -1,3 +1,11 @@
-﻿ class BootUp {
+﻿module ModernFx {
+    export interface IScope {
+        statusText: string;
+    } 
      
- }
+    export class BootUp {
+        constructor($scope: IScope) {
+            $scope.statusText = "Hello from TypeScript + AngularJS";
+        }
+    }
+}
