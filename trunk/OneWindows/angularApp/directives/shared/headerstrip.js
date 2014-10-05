e8d3e32d491bddc8b25bf34229da80d279f6ed45
Jose Fajardo <fajardo_jf@hotmail.com>
2014-10-05 17:50:26 +1100

    styling for a news site

---
+++ trunk/OneWindows/angularApp/directives/shared/headerstrip.js
@@ -0,0 +1,16 @@
+﻿app.directive('cstHeaderStrip', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/angularApp/partials/HeaderStrip.html',
+        link: function (scope, element, attrs) {
+            var menuItems = element.find("div");
+            //menuItems.on('click', function () {
+            //    menuItems.removeClass('active');
+            //    $(this).addClass('active');
+            //});
+
+            
+        }
+    };
+});
