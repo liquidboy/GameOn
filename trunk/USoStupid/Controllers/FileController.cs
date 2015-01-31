cf2a9d2ff854c377ca7577d43bfb8c3d1198e17a
Jose Fajardo <fajardo_jf@hotmail.com>
2015-01-31 15:40:53 +1100

    azure storage

---
+++ trunk/USoStupid/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace USoStupid.Controllers
+{
+    public class FileController : ApiController
+    {
+
+        public File Get(int id)
+        {
+            
+
+            return new File() { Id = 1, Name = "test.jpg" };
+        }
+
+        public int Post(string content, string fileName)
+        {
+            return 0;
+        }
+
+    }
+
+
+    public class File
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ trunk/USoStupid/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace USoStupid.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        public User Get(int id)
+        {
+            return new User { Id = id, Name = "Jose Fajardo" };
+        }
+
+    }
+
+
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
