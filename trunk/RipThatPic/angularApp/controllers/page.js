b913ebab7d47af7ddceb3d0af124cbc5792c0c52
jose.fajardo <fajardo_jf@hotmail.com>
2015-06-29 22:39:10 +1000

    reading from url in page controller

--- trunk/RipThatPic/angularApp/controllers/page.js
+++ trunk/RipThatPic/angularApp/controllers/page.js
@@ -3,19 +3,22 @@ var Application;
     var Controllers;
     (function (Controllers) {
         var PageCtrl = (function () {
-            function PageCtrl($scope, $rootScope, serviceHelperSvc, dataSvc, instanceFactory, authService) {
+            function PageCtrl($scope, $rootScope, serviceHelperSvc, dataSvc, instanceFactory, authService, location) {
                 this.$scope = $scope;
                 this.$rootScope = $rootScope;
                 this.serviceHelperSvc = serviceHelperSvc;
                 this.dataSvc = dataSvc;
                 this.instanceFactory = instanceFactory;
                 this.authService = authService;
+                this.location = location;
+                var grouping = location.search()["grouping"];
+                var name = location.search()["name"];
             }
             return PageCtrl;
         })();
         Controllers.PageCtrl = PageCtrl;
         var myapp = angular.module('bootstrapApp');
-        myapp.controller("PageCtrl", ["$scope", "$rootScope", "serviceHelperSvc", "dataSvc", "instanceFactory", "authSvc", PageCtrl]);
+        myapp.controller("PageCtrl", ["$scope", "$rootScope", "serviceHelperSvc", "dataSvc", "instanceFactory", "authSvc", "$location", PageCtrl]);
     })(Controllers = Application.Controllers || (Application.Controllers = {}));
 })(Application || (Application = {}));
 //# sourceMappingURL=page.js.map
--- trunk/RipThatPic/angularApp/controllers/page.ts
+++ trunk/RipThatPic/angularApp/controllers/page.ts
@@ -10,13 +10,16 @@
             public serviceHelperSvc: Application.Services.IServiceHelper,
             public dataSvc: Application.Services.IData,
             public instanceFactory: Application.Services.IInstanceFactory,
-            public authService: Application.Services.IAuthService) {
-
+            public authService: Application.Services.IAuthService,
+            public location: ng.ILocationService) {
+            
+            var grouping: string = location.search()["grouping"];
+            var name: string = location.search()["name"];
         }
         
 
         
     }
     var myapp: ng.IModule = angular.module('bootstrapApp');
-    myapp.controller("PageCtrl", ["$scope", "$rootScope", "serviceHelperSvc", "dataSvc", "instanceFactory", "authSvc", PageCtrl]);
+    myapp.controller("PageCtrl", ["$scope", "$rootScope", "serviceHelperSvc", "dataSvc", "instanceFactory", "authSvc", "$location", PageCtrl]);
 }
