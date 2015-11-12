8091dcb31732e661f11a5caa147160cb41188d01
Jose Fajardo <fajardo_jf@hotmail.com>
2015-11-12 20:23:22 +1100

    paypal integration

---
+++ trunk/sandbox/demo01/NanoServerApplication.cpp
@@ -0,0 +1,12 @@
+// NanoServerApplication.cpp : Defines the entry point for the NanoServer console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello, Nano Server!\n");
+	return 0;
+}
+
