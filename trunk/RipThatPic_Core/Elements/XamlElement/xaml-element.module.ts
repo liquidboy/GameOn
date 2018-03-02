7118768b2b448416c1544ec8c105cfb7113b70f8
jose.fajardo <fajardo_jf@hotmail.com>
2018-03-02 12:31:43 +1100

    cleaning up xaml-element module

---
+++ trunk/RipThatPic_Core/Elements/XamlElement/xaml-element.module.ts
@@ -0,0 +1,39 @@
+﻿import {
+    Component, ComponentFactory, NgModuleRef, NgModule, CompilerFactory,
+    NgModuleFactory, NgModuleFactoryLoader, Type, destroyPlatform
+} from '@angular/core';
+import { createNgElementConstructor, NgElementConfig, NgElementConstructor, getConfigFromComponentFactory,  } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Subject } from 'rxjs/Subject';
+import { XamlElement } from './xaml-element';
+
+@NgModule({
+    //bootstrap: [XamlElement],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        
+    ],
+    declarations: [XamlElement],
+    entryComponents: [XamlElement]
+})
+export class XamlElementModule {
+    constructor(private moduleRef: NgModuleRef<any>) {
+        console.log("xaml-element.ts->XamlElementModule.constructor");
+        console.log(moduleRef);
+
+        const componentFactory = moduleRef.componentFactoryResolver.resolveComponentFactory<XamlElement>(XamlElement);
+        console.log(componentFactory);
+        const ngElementConfig = getConfigFromComponentFactory(componentFactory, moduleRef.injector);
+        const ngElementCtor = createNgElementConstructor(ngElementConfig); 
+        console.log(ngElementCtor);
+   
+        customElements.define(componentFactory.selector, ngElementCtor);
+    }
+    ngDoBootstrap() {
+        console.log("xaml-element.module.ts->XamlElementModule.ngDoBootstrap");
+    }
+    
+}
--- trunk/RipThatPic_Core/Elements/boot.browser.ts
+++ trunk/RipThatPic_Core/Elements/boot.browser.ts
@@ -1,47 +1,7 @@
 import 'reflect-metadata';
 import 'zone.js';
 import 'bootstrap';
-//import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './XamlElement/app.module';
+import { XamlElementModule } from './XamlElement/xaml-element.module';
 
-
-////import { ComponentFactory } from '@angular/core';
-////import { createNgElementConstructor, NgElementDelegateFactory, NgElementConstructor } from '@angular/elements';
-////import { XamlElement } from './XamlElement/xaml-element';
-
-//if (module.hot) {
-//    module.hot.accept();
-//    module.hot.dispose(() => {
-//        // Before restarting the app, we create a new root element and dispose the old one
-//        const oldRootElem = document.querySelector('app');
-//        const newRootElem = document.createElement('app');
-//        oldRootElem!.parentNode!.insertBefore(newRootElem, oldRootElem);
-//        modulePromise.then(appModule => appModule.destroy());
-//    });
-//} else {
-//    enableProdMode();
-//}
-
-// Note: @ng-tools/webpack looks for the following expression when performing production
-// builds. Don't change how this line looks, otherwise you may break tree-shaking.
-
-const modulePromise = platformBrowserDynamic().bootstrapModule(AppModule);
-
-////const modulePromise = platformBrowserDynamic()
-////    .bootstrapModule(AppModule)
-////    .then(moduleRef => {
-////        let _delegateFactory: NgElementDelegateFactory<any>;
-////        let _ngElementCtor: NgElementConstructor<any>;
-////        let _componentFactory: ComponentFactory<XamlElement>;
-
-////        console.log(moduleRef);
-////        _delegateFactory = new NgElementDelegateFactory<any>(moduleRef.injector);
-////        console.log(_delegateFactory);
-////        _componentFactory = moduleRef.componentFactoryResolver.resolveComponentFactory(XamlElement);
-////        console.log(_componentFactory);
-////        _ngElementCtor = createNgElementConstructor(_componentFactory, { delegateFactory: _delegateFactory });
-////        console.log(_ngElementCtor);
-////        customElements.define(_componentFactory.selector, _ngElementCtor);
-
-////    });
+const modulePromise = platformBrowserDynamic().bootstrapModule(XamlElementModule);
