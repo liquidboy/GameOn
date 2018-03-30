65a3dbc4cd465b903403d850190f12bd05d7efcd
jose.fajardo <fajardo_jf@hotmail.com>
2018-03-31 09:46:17 +1100

    read original innerhtml from within component

--- trunk/RipThatPic_Core/Elements/Canvas/xaml-canvas.ts
+++ trunk/RipThatPic_Core/Elements/Canvas/xaml-canvas.ts
@@ -1,14 +1,38 @@
-﻿import { Component, Input, NgModule, ViewEncapsulation } from '@angular/core';
+﻿import { Component, Input, NgModule, ViewEncapsulation, ViewChild, ElementRef, SimpleChanges, OnChanges,AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'xaml-canvas',
     template: `
-        <h1>xaml canvas</h1>
+        <canvas #rootElement></canvas>
+        {{tablesrc}}
+        <span #contentElement [hidden]="true"><ng-content></ng-content></span>
     `,
 })
-export class XamlCanvas{
+export class XamlCanvas implements OnChanges, AfterViewInit{
     @Input() name: string = 'World!';
+    @Input() tablesrc: string;
+    @ViewChild("rootElement") rootElement: ElementRef;
+    @ViewChild("contentElement") contentElement: ElementRef;
+
+    private tableData: any;
+
     constructor() {
         console.log("XamlCanvas.constructor");
+        //console.log(this.rootElement);
+        //console.log(this.contentElement);
+    }
+
+    ngAfterViewInit() {
+        this.tableData = JSON.parse(this.contentElement.nativeElement.innerText);
+        console.log(this.tableData);
+    }
+
+    ngOnChanges(changes: SimpleChanges): any {
+        if ('tablesrc' in changes) {
+            if (changes.tablesrc.previousValue !== changes.tablesrc.currentValue) {
+                console.log(`tablesrc change detected : ${changes.tablesrc.currentValue}`);
+            }
+            console.log(changes);
+        }
     }
 }
