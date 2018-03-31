6574ed4342159ac2c8a555cd5b5392a948ff21c7
jose.fajardo <fajardo_jf@hotmail.com>
2018-03-31 11:00:27 +1100

    handling content by a new enum

--- trunk/RipThatPic_Core/Elements/Canvas/xaml-canvas.ts
+++ trunk/RipThatPic_Core/Elements/Canvas/xaml-canvas.ts
@@ -9,8 +9,11 @@
     `,
 })
 export class XamlCanvas implements OnChanges, AfterViewInit{
-    @Input() name: string = 'World!';
+
+    // note: these are projected properties from the original root element 'xaml-canvas' and are case sensitive, KEEP THEM LOWERCASE OR SPEND HOURS WORKING OUT BUGS
     @Input() tablesrc: string;
+    @Input() contenttype: ContentType = ContentType.None;
+
     @ViewChild("rootElement") rootElement: ElementRef;
     @ViewChild("contentElement") contentElement: ElementRef;
 
@@ -18,13 +21,18 @@ export class XamlCanvas implements OnChanges, AfterViewInit{
 
     constructor() {
         console.log("XamlCanvas.constructor");
-        //console.log(this.rootElement);
-        //console.log(this.contentElement);
+
+        // note: too early to get to this stuff
+        // console.log(this.rootElement);
+        // console.log(this.contentElement);
     }
 
     ngAfterViewInit() {
         this.tableData = JSON.parse(this.contentElement.nativeElement.innerText);
         console.log(this.tableData);
+        console.log(this.tablesrc);
+        console.log(this.contenttype);
+        console.log(this.contenttype === ContentType.Datagrid_JSON);
     }
 
     ngOnChanges(changes: SimpleChanges): any {
@@ -32,7 +40,21 @@ export class XamlCanvas implements OnChanges, AfterViewInit{
             if (changes.tablesrc.previousValue !== changes.tablesrc.currentValue) {
                 console.log(`tablesrc change detected : ${changes.tablesrc.currentValue}`);
             }
-            console.log(changes);
         }
+
+        if ('contenttype' in changes) {
+            switch (changes.contenttype.currentValue.toLowerCase()) {
+                case 'datagrid-json': this.contenttype = ContentType.Datagrid_JSON; break;
+                case 'xaml': this.contenttype = ContentType.XAML; break;
+                default: this.contenttype = ContentType.None;
+            }
+        }
+        console.log(changes);
     }
+}
+
+enum ContentType {
+    Datagrid_JSON = 1,
+    None,
+    XAML
 }
